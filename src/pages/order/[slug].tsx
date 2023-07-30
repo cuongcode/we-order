20a2f52c4831c51ec25eeb2f50d66cd6e46185d1
cuongcode <congcuongnguyen.dev@gmail.com>
2023-07-30 12:29:52 +0700

    change production link

--- src/pages/order/[slug].tsx
+++ src/pages/order/[slug].tsx
@@ -229,7 +229,7 @@ const SharedLink = ({ orderId }: { orderId: string }) => {
     <div className="flex flex-col gap-2">
       <div>Share this link :</div>
       <div className="flex items-center gap-2 border-2 border-gray-300 rounded-lg w-fit py-1 px-3">
-        <div>https://weorder/order/{orderId}</div>
+        <div>https://we-order-omega.vercel.app/order/{orderId}</div>
         <button type="button" onClick={_copyClipboard}>
           <ClipboardDocumentIcon className="w-5 h-5" />
         </button>
