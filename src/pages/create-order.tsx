8340e144e31ed24344d14709ad8c10465d62020a
cuongcode <congcuongnguyen.dev@gmail.com>
2023-08-01 23:49:10 +0700

    clean create order page

--- src/pages/create-order.tsx
+++ src/pages/create-order.tsx
@@ -2,29 +2,16 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { collection, addDoc, onSnapshot } from "firebase/firestore";
 import { db } from "@/firebase";
-import { Order, Menu } from "@/types";
+import { Order } from "@/types";
 import Router from "next/router";
 import { Main } from "@/templates/Main";
 import { Meta } from "@/layouts/Meta";
 
 const CreateOrderPage = () => {
   const [orders, setOrders] = useState<Order[]>([]);
-  // const [user, setUser] = useState<User>({
-  //   id: "",
-  //   name: "",
-  //   momo: "",
-  //   menus: [],
-  // });
-  const [menus, setMenus] = useState<Menu[]>([]);
-  const [selectedMenu, setSelectedMenu] = useState<Menu>({
-    id: "",
-    name: "",
-    link: "",
-  });
 
   useEffect(() => {
     _fetchOrders();
-    _fetchMenus();
   }, []);
 
   const _fetchOrders = async () => {
@@ -37,25 +24,7 @@ const CreateOrderPage = () => {
     });
   };
 
-  const _fetchMenus = async () => {
-    onSnapshot(collection(db, "menus"), (snapshot) => {
-      const updatedMenus = snapshot.docs.map((doc: any) => {
-        return { ...doc.data(), id: doc.id };
-      });
-      setMenus(updatedMenus);
-    });
-  };
-
   const _createOrder = async () => {
-    // if (user.name != '' && user.momo != '' && selectedMenu.link != '') {
-    //   const newOrder = {
-    //     shipFee: 0,
-    //     discount: 0,
-    //     shopOwnerName: '',
-    //     shopOwnerMomo: '',
-    //     selectedMenuName: '',
-    //     selectedMenuLink: '',
-    //   };
     const newOrder = {
       shipFee: 0,
       discount: 0,
@@ -70,15 +39,6 @@ const CreateOrderPage = () => {
   return (
     <Main meta={<Meta title="WeOrder" description="" />}>
       <div className="flex flex-col gap-4 w-96 bg-green-100">
-        <div>Anonymous</div>
-        {/* <AnonymousUser user={user} setUser={setUser} /> */}
-        <div>Menu:</div>
-        <AddMenu />
-        <MenuList menus={menus} setSelectedMenu={setSelectedMenu} />
-        <div className="flex flex-col">
-          <div>Selected Menu</div>
-          <div>{selectedMenu.name}</div>
-        </div>
         <button type="button" onClick={_createOrder}>
           Create Order
         </button>
@@ -89,43 +49,6 @@ const CreateOrderPage = () => {
 };
 
 export default CreateOrderPage;
-
-// const AnonymousUser = ({
-//   user,
-//   setUser,
-// }: {
-//   user: User;
-//   setUser: (user: User) => void;
-// }) => {
-//   const _onNameChange = (e: any) => {
-//     const updatedUser = { ...user, name: e.target.value };
-//     setUser(updatedUser);
-//   };
-
-//   const _onMomoChange = (e: any) => {
-//     const updatedUser = { ...user, momo: e.target.value };
-//     setUser(updatedUser);
-//   };
-
-//   return (
-//     <div className="flex flex-col">
-//       <div>Name</div>
-//       <input
-//         className="border-2"
-//         type="text"
-//         value={user.name}
-//         onChange={(e) => _onNameChange(e)}
-//       />
-//       <div>Momo</div>
-//       <input
-//         className="border-2"
-//         type="string"
-//         value={user.momo}
-//         onChange={(e) => _onMomoChange(e)}
-//       />
-//     </div>
-//   );
-// };
 
 const OrderList = ({ orders }: { orders: Order[] }) => {
   const _openOrder = (id: string | null) => {
@@ -140,76 +63,6 @@ const OrderList = ({ orders }: { orders: Order[] }) => {
           onClick={() => _openOrder(order.id)}
         >
           {order.id}
-        </button>
-      ))}
-    </div>
-  );
-};
-
-const AddMenu = () => {
-  const [menu, setMenu] = useState<Menu>({ id: "", name: "", link: "" });
-
-  const _onNameChange = (e: any) => {
-    const updatedMenu = { ...menu, name: e.target.value };
-    setMenu(updatedMenu);
-  };
-
-  const _onLinkChange = (e: any) => {
-    const updatedMenu = { ...menu, link: e.target.value };
-    setMenu(updatedMenu);
-  };
-
-  const _addMenu = async () => {
-    if (menu.name != "" && menu.link != "") {
-      await addDoc(collection(db, "menus"), {
-        name: menu.name,
-        link: menu.link,
-      });
-      setMenu({ id: "", name: "", link: "" });
-    }
-    return;
-  };
-
-  return (
-    <div className="flex flex-col">
-      <div>Name :</div>
-      <input
-        className="border-2"
-        type="text"
-        value={menu.name}
-        onChange={(e) => _onNameChange(e)}
-      />
-      <div>Link :</div>
-      <input
-        className="border-2"
-        type="text"
-        value={menu.link}
-        onChange={(e) => _onLinkChange(e)}
-      />
-      <button type="submit" onClick={_addMenu}>
-        Add Menu
-      </button>
-    </div>
-  );
-};
-
-const MenuList = ({
-  menus,
-  setSelectedMenu,
-}: {
-  menus: Menu[];
-  setSelectedMenu: (menu: Menu) => void;
-}) => {
-  return (
-    <div className="flex flex-col">
-      {menus.map((menu: Menu) => (
-        <button
-          type="button"
-          key={menu.id}
-          onClick={() => setSelectedMenu(menu)}
-        >
-          {menu.name}
-          {menu.link}
         </button>
       ))}
     </div>
