7ef4d129750780b239943f7a84bf7659c7d7e2d7
cuongcode <congcuongnguyen.dev@gmail.com>
2023-08-01 23:28:01 +0700

    add menu function

--- src/pages/order/[slug].tsx
+++ src/pages/order/[slug].tsx
@@ -21,6 +21,8 @@ import {
   collection,
   orderBy,
   query as firestoreQuery,
+  updateDoc,
+  addDoc,
 } from "firebase/firestore";
 import { db } from "@/firebase";
 
@@ -118,17 +120,17 @@ const MenusDropdown = ({ order }: { order: Order }) => {
       {isDropdown ? (
         <div
           ref={dropdownRef}
-          className="absolute top-12 flex flex-col gap-2 bg-gray-200 rounded-lg p-2"
+          className="absolute top-12 flex flex-col gap-2 bg-gray-200 rounded-lg p-2 w-full"
         >
           <AddMenuForm />
-          <Menus />
+          <Menus order={order} />
         </div>
       ) : null}
     </div>
   );
 };
 
-const Menus = () => {
+const Menus = ({ order }: { order: Order }) => {
   const [menus, setMenus] = useState<Menu[]>([]);
 
   useEffect(() => {
@@ -144,8 +146,12 @@ const Menus = () => {
     });
   };
 
-  const _selectMenu = () => {
-    //
+  const _selectMenu = async (menuName: string, menuLink: string) => {
+    const docRef = doc(db, "orders", order.id);
+    await updateDoc(docRef, {
+      selectedMenuName: menuName,
+      selectedMenuLink: menuLink,
+    });
   };
 
   return (
@@ -155,7 +161,7 @@ const Menus = () => {
           className="px-3 py-1 rounded-lg bg-white hover:bg-gray-400"
           type="button"
           key={menu.id}
-          onClick={_selectMenu}
+          onClick={() => _selectMenu(menu.name, menu.link)}
         >
           {menu.name}
         </button>
@@ -165,23 +171,40 @@ const Menus = () => {
 };
 
 const AddMenuForm = () => {
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+
+  const _addMenu = async () => {
+    if (name !== "" && link !== "") {
+      await addDoc(collection(db, "menus"), { name: name, link: link });
+      setName("")
+      setLink("")
+    }
+    return
+  };
+
   return (
     <div className="flex gap-2 items-center">
       <div>Name:</div>
       <input
-        className="px-1 rounded-md border-2 hover:border-gray-600 w-28"
+        className="px-1 rounded-md border-2 hover:border-gray-600 w-48"
         type="text"
         placeholder="menu name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <div>Link:</div>
       <input
-        className="px-1 rounded-md border-2 hover:border-gray-600 w-56"
+        className="px-1 rounded-md border-2 hover:border-gray-600 w-48"
         type="text"
         placeholder="paste link here"
+        value={link}
+        onChange={(e) => setLink(e.target.value)}
       />
       <button
         type="button"
         className="p-1 bg-white rounded-md hover:bg-gray-400"
+        onClick={_addMenu}
       >
         <PlusIcon className="w-4 h-4" />
       </button>
