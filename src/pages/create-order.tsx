7a2dcc092fb8d677ce8603308b96734024de8bd6
cuongcode <congcuongnguyen.dev@gmail.com>
2023-08-11 11:51:23 +0700

    add please select menu error

--- src/pages/create-order.tsx
+++ src/pages/create-order.tsx
@@ -118,10 +118,19 @@ const CreateOrderPage = () => {
 export default CreateOrderPage;
 
 const NewOrderButton = ({ selectedMenu }: { selectedMenu: Menu }) => {
+  const [error, setError] = useState<any>({});
   const { currentUser } = useSelector(selector.user);
 
   const _createOrder = async () => {
-    if (selectedMenu.link !== '' && currentUser) {
+    if (currentUser) {
+      if (selectedMenu.link === '') {
+        const newError = {
+          ...error,
+          missingMenuLink: 'Please select a menu',
+        };
+        setError(newError);
+        return;
+      }
       const newOrder = {
         timestamp: serverTimestamp(),
         shipFee: 0,
@@ -133,17 +142,23 @@ const NewOrderButton = ({ selectedMenu }: { selectedMenu: Menu }) => {
         uid: currentUser?.uid,
       };
       await addDoc(collection(db, 'orders'), newOrder);
+      setError({});
     }
   };
 
   return (
-    <button
-      type="button"
-      onClick={_createOrder}
-      className="rounded-lg bg-gray-200 py-2 hover:bg-gray-400"
-    >
-      New Order
-    </button>
+    <div className="relative">
+      <button
+        type="button"
+        onClick={_createOrder}
+        className="w-full rounded-lg bg-gray-200 py-2 hover:bg-gray-400"
+      >
+        New Order
+      </button>
+      {selectedMenu.link === '' ? (
+        <div className="absolute text-red-500">{error.missingMenuLink}</div>
+      ) : null}
+    </div>
   );
 };
 
