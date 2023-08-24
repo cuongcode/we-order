a7508f75e35e2eeab5a5682eba80387f80180b45
cuongcode <congcuongnguyen.dev@gmail.com>
2023-08-25 00:10:46 +0700

    fix type error

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,6 @@ export * from './BankInfo';
 export * from './DrinkTableData';
 export * from './DrinkTableRow';
 export * from './Menu';
-export * from './MenuImage';
 export * from './Order';
 export * from './TransferInfo';
 export * from './User';
