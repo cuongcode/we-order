a5399292ed00fb4542d5746cb781e0108af6b8f0
cuongcode <congcuongnguyen.dev@gmail.com>
2023-11-05 22:52:34 +0700

    redirect page if no match name on collection

--- src/pages/no-sign-in-order/[slug].tsx
+++ src/pages/no-sign-in-order/[slug].tsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import {
+  collection,
+  onSnapshot,
+  query as firestoreQuery,
+} from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+
+import { db } from '@/firebase';
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
 
 const NoSignInOrderPage = ({ query }: { query: any }) => {
+  const [orderNamePool, setorderNamePool] = useState<any>([]);
+  useEffect(() => {
+    _fetchNoSignInOrder();
+  }, []);
   // const _fetchOrder = () => {
   //   const docRef = doc(db, 'orders', query?.slug);
   //   onSnapshot(docRef, (document) => {
@@ -18,12 +31,26 @@ const NoSignInOrderPage = ({ query }: { query: any }) => {
   //     dispatch(OrderActions.setOrder(newOrder));
   //   });
   // };
-
+  const _fetchNoSignInOrder = async () => {
+    const q = firestoreQuery(collection(db, 'no_sign_in_orders'));
+    onSnapshot(q, (snapshot) => {
+      const updatedNoSignInOrder = snapshot.docs.map((_doc: any) => {
+        return _doc.id;
+      });
+      setorderNamePool(updatedNoSignInOrder);
+    });
+  };
   return (
-    <div>
-      <div>Work in progress</div>
-      <div>{query.slug}</div>
-    </div>
+    <Main meta={<Meta title="WeOrder" description="" />}>
+      {!orderNamePool.includes(query.slug) ? (
+        <div>Page not found</div>
+      ) : (
+        <div>
+          <div>Work in progress</div>
+          <div>{query.slug}</div>
+        </div>
+      )}
+    </Main>
   );
 };
 
