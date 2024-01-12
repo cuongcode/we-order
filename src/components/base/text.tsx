df5112bd9df8d9209bcc8104b8d87b780edb26c5
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-12 22:36:02 +0700

    small style text base button

--- src/components/base/text.tsx
+++ src/components/base/text.tsx
@@ -19,10 +19,10 @@ export const Text: FC<TextProps> = (props) => {
 
 export type TextPresets = keyof typeof presetClassName;
 const presetClassName = {
-  h1: 'text-[44px]',
-  h1p: 'text-[44px] font-medium',
-  h2: 'text-4xl',
-  h2p: 'text-4xl font-semibold',
+  h1: 'text-[34px]',
+  h1p: 'text-[34px] font-medium',
+  h2: 'text-[24px]',
+  h2p: 'text-[24px] font-semibold',
   h3: 'text-2xl',
   h3p: 'text-2xl font-medium',
   h4: 'text-lg',
