1bc609f1a091201051f96fd2a33ab2edeaa8b788
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-13 13:06:22 +0700

    style shop owner profile

--- src/components/pages/no-sign-in-order/shop-owner-profile.tsx
+++ src/components/pages/no-sign-in-order/shop-owner-profile.tsx
@@ -3,6 +3,7 @@ import { doc, updateDoc } from 'firebase/firestore';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
 
+import { Text } from '@/components/base';
 import { Portal } from '@/components/common';
 import { db } from '@/firebase';
 import { useCheckClickOutside } from '@/hooks';
@@ -12,13 +13,11 @@ import { selector } from '@/redux';
 import { CheckPasswordPortal } from './check-password-portal';
 
 export const ShopOwnerProfile = () => {
+  const { noSignInOrder } = useSelector(selector.order);
   return (
-    <div className="relative flex h-full w-full flex-col items-center rounded-3xl border-2 bg-white p-3 drop-shadow-md">
-      <div className="font-bold">SHOP OWNER</div>
+    <div className="relative flex h-full w-full flex-col items-center rounded-3xl bg-slate-800 p-3">
       <ShopOwnerImage />
-      <div className="mt-1">
-        <ShopOwnerNameInput />
-      </div>
+      <Text text={noSignInOrder?.nickname} className="self-center" />
       <CloseOrderButton />
     </div>
   );
@@ -36,7 +35,7 @@ const ShopOwnerImage = () => {
   };
 
   return (
-    <div className="relative rounded-full bg-gray-500 p-1">
+    <div className="relative rounded-full bg-slate-800 p-1">
       <button
         onClick={_onOpen}
         className="flex h-20 w-20 rounded-full bg-gray-200"
@@ -67,18 +66,6 @@ const ShopOwnerImage = () => {
   );
 };
 
-const ShopOwnerNameInput = () => {
-  const { noSignInOrder } = useSelector(selector.order);
-
-  return (
-    <div className="relative">
-      <div className="h-6 w-20 rounded-md border-2 border-white text-center">
-        {noSignInOrder?.nickname}
-      </div>
-    </div>
-  );
-};
-
 const CloseOrderButton = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { noSignInOrder } = useSelector(selector.order);
@@ -98,7 +85,7 @@ const CloseOrderButton = () => {
   return (
     <>
       <button
-        className="absolute top-40 flex w-36 items-center gap-2 rounded-lg bg-gray-200 p-1 px-2 hover:bg-gray-400"
+        className="absolute top-40 flex w-36 items-center gap-2 rounded-lg bg-slate-800 p-1 px-2 hover:bg-gray-400"
         onClick={_onCheckPassword}
       >
         {noSignInOrder.isClosed ? (
