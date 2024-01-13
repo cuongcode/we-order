3cff2aea95da8b4e8852d3e54f63b3b93d0af563
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-13 12:41:43 +0700

    style sign in page

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -4,8 +4,9 @@ import router from 'next/router';
 import React from 'react';
 import { useDispatch } from 'react-redux';
 
+import { Button, Text } from '@/components/base';
 import { auth, db, provider } from '@/firebase';
-import { Icons, LogoImages } from '@/images';
+import { Icons } from '@/images';
 import { Meta } from '@/layouts/Meta';
 import { UserActions } from '@/redux';
 import { Main } from '@/templates/Main';
@@ -60,38 +61,31 @@ const SignIn = () => {
 
   return (
     <Main meta={<Meta title="WeOrder" description="" />}>
-      <div className="m-auto flex max-w-5xl flex-col font-semibold">
-        <img
-          className="mb-3 mt-12 w-[400px] self-center"
-          src={LogoImages.title_logo.src}
-          alt="title-logo"
+      <div className="m-auto flex max-w-xl flex-col">
+        <Text preset="h1b" text="Sign In" />
+        <Text
+          preset="p2"
+          className="font-light"
+          text="Sign in with google account or as anonymous"
         />
-        <div className="text-center text-lg">
-          An easy way to order drink and food together
-        </div>
-        <div className="text-center text-lg">Order together is fun !</div>
-        <div className="mt-12 flex flex-col items-center justify-center gap-3">
-          <button
+        <div className="mt-6 flex flex-col items-center justify-center gap-3 self-stretch">
+          <Button
             onClick={_onSignIn}
-            className="flex w-60 flex-col items-center gap-2 rounded-2xl bg-gray-200 py-3 hover:bg-gray-400"
+            className="flex items-center justify-center gap-2 self-stretch bg-slate-800"
           >
-            <div className="w-10 rounded-full">
-              <img src={Icons.google_icon.src} alt="user-icon" />
-            </div>
-            <div>Sign in with Google</div>
-          </button>
-          {/* <button className="flex w-60 flex-col items-center gap-2 rounded-2xl bg-gray-200 py-3 hover:bg-gray-400">
-            <div className="w-10 rounded-full">
-              <img src={Icons.user_icon.src} alt="user-icon" />
-            </div>
-            <div>Sign in as Anonymous</div>
-          </button> */}
-          <button
-            className="flex w-60 flex-col items-center gap-2 rounded-2xl bg-gray-200 py-3 hover:bg-gray-400"
+            <img
+              className="w-5"
+              src={Icons.google_icon.src}
+              alt="google-icon"
+            />
+            <Text preset="p2" text="Sign in with Google" />
+          </Button>
+          <Button
             onClick={_onNoSignInOrder}
+            className="flex items-center justify-center gap-2 self-stretch"
           >
-            <div>No Sign In Order</div>
-          </button>
+            <Text preset="p2" text="Sign in as anonymous" />
+          </Button>
         </div>
       </div>
     </Main>
