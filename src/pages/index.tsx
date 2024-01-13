6293dbacb9964b5d836be3afa72afe7e48585158
cuongcode <congcuongnguyen.dev@gmail.com>
2024-01-13 12:19:37 +0700

    homepage style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,9 @@
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
-import { Button } from '@/components/base';
+import { Button, Text } from '@/components/base';
 import { FeatureLine } from '@/components/pages/home';
 import { FEATURE_LIST } from '@/components/pages/home/data';
-import { LogoImages } from '@/images';
 import { Meta } from '@/layouts/Meta';
 import { selector } from '@/redux';
 import { Main } from '@/templates/Main';
@@ -15,18 +14,10 @@ const LandingPage = () => {
   const router = useRouter();
 
   return (
-    <Main meta={<Meta title="WeOrder" description="" />}>
-      <div className="m-auto flex max-w-5xl flex-col font-semibold">
-        <img
-          className="mb-3 mt-12 w-1/2 self-center"
-          src={LogoImages.title_logo.src}
-          alt="title-logo"
-        />
-        <div className="text-center text-lg">
-          An easy way to order drink and food together
-          <br />
-          Order together is fun !
-        </div>
+    <Main meta={<Meta title="We Order" description="" />}>
+      <div className="m-auto flex max-w-5xl flex-col items-center">
+        <Text preset="h1b" text="Order together is fun !" />
+        <Text preset="p1" text="An easy way to order drink and food together" />
         <div className="m-auto mt-24 flex w-fit list-disc flex-col gap-2">
           <div>Features :</div>
           {FEATURE_LIST.map((feature) => (
@@ -34,7 +25,7 @@ const LandingPage = () => {
           ))}
         </div>
         <Button
-          className="mt-5"
+          className="mt-24 self-stretch"
           onClick={() =>
             router.push(currentUser ? '/create-order' : '/sign-in')
           }
